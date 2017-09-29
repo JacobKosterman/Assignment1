568899d6d6955695d0e548dab430dbef4c121733
Jacob Kosterman <slumberstudios@gmail.com>
2017-09-29 09:08:07 -0400

    Landscape Functionality

--- app/src/main/java/com/example/jkosterman6222/assignment1/ImagePreviewPage.java
+++ app/src/main/java/com/example/jkosterman6222/assignment1/ImagePreviewPage.java
@@ -1,7 +1,11 @@
 package com.example.jkosterman6222.assignment1;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ImagePreviewPage extends AppCompatActivity {
 
@@ -10,4 +14,14 @@ public class ImagePreviewPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_preview_page);
     }
+
+    public void captureImage (View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Picture Has Been Taken");
+        AlertDialog dialog = builder.create();
+        builder.create().show();
+
+    };
+
 }
