8626e34bec47aff7e1b23000230c8721a2e3a6f6
Jacob Kosterman <slumberstudios@gmail.com>
2017-11-30 17:59:38 -0500

    Started Database

---
+++ app/src/main/java/com/example/jkosterman6222/assignment1/User.java
@@ -0,0 +1,26 @@
+package com.example.jkosterman6222.assignment1;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Created by vogella on 06.09.17.
+ */
+@Entity
+public class User {
+
+    @PrimaryKey
+    public final int id;
+    public String name;
+    public int level;
+    public long skillPoints;
+
+
+    public User(int id, String name, long skillPoints) {
+        this.id = id;
+        this.name = name;
+        this.skillPoints  = skillPoints;
+        this.level = 0;
+    }
+
+}
